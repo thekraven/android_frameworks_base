1f67b491dff4d1c1b62d614a8f665c37dc505c0d
thekraven <savethestyle1@gmail.com>
2012-06-25 18:13:25 -0500

    Revert "Commits from linaro to fix builds with newer toolchains and options"
    This reverts commit 80be4ed80987e5a0ee5c34a3afad3056456c7ff7.

--- opengl/libs/Android.mk
+++ opengl/libs/Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-FLTO_FLAG=$(call cc-option,"-flto", )
-
 ###############################################################################
 # Build META EGL library
 #
@@ -19,9 +17,8 @@ LOCAL_SRC_FILES:= 	       \
 	EGL/getProcAddress.cpp.arm \
 	EGL/Loader.cpp 	       \
 
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
 LOCAL_SHARED_LIBRARIES += libcutils libutils libGLESv2_dbg
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libEGL
 LOCAL_LDFLAGS += -Wl,--exclude-libs=ALL
 LOCAL_SHARED_LIBRARIES += libdl
@@ -78,8 +75,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := GLES_CM/gl.cpp.arm
 
 LOCAL_SHARED_LIBRARIES += libcutils libEGL
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libGLESv1_CM
 
 LOCAL_SHARED_LIBRARIES += libdl
@@ -107,9 +103,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := GLES2/gl2.cpp.arm
 
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
 LOCAL_SHARED_LIBRARIES += libcutils libEGL
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libGLESv2
 
 LOCAL_SHARED_LIBRARIES += libdl
@@ -149,8 +144,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := ETC1/etc1.cpp
 
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libETC1
 
 include $(BUILD_SHARED_LIBRARY)
