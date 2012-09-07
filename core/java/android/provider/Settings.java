9ba4b14ebc3c4bdb50c2e108225bd0e5006f267d
Peter Gregus <pete.gregus@gmail.com>
2012-09-07 22:30:35 +0200

    Screenshot settings 1/2
    Change-Id: I55db1c17c89301c4a09aa9768f3f97199135707c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2665,6 +2665,18 @@ public final class Settings {
         public static final String UI_MENU_BUTTON_BEHAVIOUR = "ui_menu_button_behaviour";
 
         /**
+         * Enable/disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+
+        /**
+         * Screenshot delay
+         * @hide
+         */
+        public static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
